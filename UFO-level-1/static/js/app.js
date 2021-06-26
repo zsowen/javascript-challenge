785e7770235ddecee238f9163c0456bc18f19eb6
zsowen <zachary.owen.10@gmail.com>
2021-06-26 14:06:47 -0400

    Initial filter and Table Function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,25 +4,29 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-console.log(tableData);
-
-data.forEach((ufoSighting) => {
+function buildTable(data) {
+  tbody.html("");
+  data.forEach((dataRow) => {
     var row = tbody.append("tr");
-    Object.entries(ufoSighting).forEach(([key, value]) => {
+    Object.entries(dataRow).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
 });
+}
+
+buildTable(tableData);
+
 
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // Select the form
 var form = d3.select("#form");
 
 // Create event handlers 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+form.on("submit", runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -37,9 +41,12 @@ function runEnter() {
   var inputValue = inputElement.property("value");
 
   console.log(inputValue);
-  // console.log(tableData);
+  console.log(tableData);
 
-  // var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue);
+  var filteredData = tableData.filter(dataRow => dataRow.datetime === inputValue);
 
-  // console.log(filteredData);
+  console.log(filteredData);
+
+buildTable(filteredData)
 };
+
