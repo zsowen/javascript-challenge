7e07c4b9446df5573edbbe5d0b013a5268bfc72d
zsowen <zachary.owen.10@gmail.com>
2021-06-26 10:52:03 -0400

    playing with filters

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,33 +12,34 @@ data.forEach((ufoSighting) => {
       var cell = row.append("td");
       cell.text(value);
     });
-  });
+});
 
-// // Select the button
-// var button = d3.select("#button");
+// Select the button
+var button = d3.select("#button");
 
-// // Select the form
-// var form = d3.select("#form");
+// Select the form
+var form = d3.select("#form");
 
-// // Create event handlers 
-// button.on("click", runEnter);
-// form.on("submit",runEnter);
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
 
-// // Complete the event handler function for the form
-// function runEnter() {
+// Complete the event handler function for the form
+function runEnter() {
 
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
   
-//   // Select the input element and get the raw HTML node
-//   var inputElement = d3.select("#ufo-form-input");
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
 
-//   // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
 
-//   console.log(inputValue);
-//   console.log(tableData);
+  console.log(inputValue);
+  // console.log(tableData);
 
-//   var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue);
+  // var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue);
 
-//   console.log(filteredData);
+  // console.log(filteredData);
+};
